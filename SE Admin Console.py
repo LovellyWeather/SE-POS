c11bb2ad40a2e25812de858132f3c45b332b6380
dirtyqwerty69 <lovellt@uni.edu>
2016-04-19 15:30:13 -0500

    Added files via upload

--- SE Admin Console.py
+++ SE Admin Console.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import os
 
 def addEmployee(Lbox, entry1):
     e1 = entry1.get()
@@ -7,6 +8,16 @@ def addEmployee(Lbox, entry1):
 
 def deleteEmployee(Lbox):
     Lbox.delete(ACTIVE)
+
+def goToFile(inventoryFile):
+    os.system(inventoryFile)
+
+def saveEmployees():
+    temp_list = list(EmployeeDirectoryList.get(0, END))
+    temp_list = [line + '\n' for line in temp_list]
+    fout = open("employees.txt", "w")
+    fout.writelines(temp_list)
+    fout.close()
 
 root = Tk()
 root.title("Admin Console")
@@ -18,6 +29,7 @@ RightFrame = Frame(root)
 RightFrame.grid(row = 0, column = 1, padx=20, pady=20)
 LowerLeftFrame = Frame(root)
 LowerLeftFrame.grid(row = 1, column = 0, padx=20, pady=20)
+
 LowerRightFrame = Frame(root)
 LowerRightFrame.grid(row = 1, column = 1)
 
@@ -31,29 +43,38 @@ EmployeeDirectory.pack()
 
 '''ListBoxes'''
 lScroll = Scrollbar(LeftFrame, orient=VERTICAL)
-InventoryList = Listbox(LeftFrame, yscrollcommand=lScroll.set)
+InventoryList = Listbox(LeftFrame, yscrollcommand=lScroll.set, width=30)
 eScroll = Scrollbar(RightFrame, orient=VERTICAL)
-EmployeeDirectoryList = Listbox(RightFrame, yscrollcommand=eScroll.set)
+EmployeeDirectoryList = Listbox(RightFrame, yscrollcommand=eScroll.set, width=30)
 
 lScroll.pack(side=RIGHT, fill=Y)
 InventoryList.pack(fill=BOTH)
 eScroll.pack(side=RIGHT, fill=Y)
 EmployeeDirectoryList.pack(fill=BOTH)
 
+file = open("posinventory.txt", 'r')
+for line in file:
+    InventoryList.insert(END, line)
+file.close()
+
+file1 = open("employees.txt", 'r')
+for line in file1:
+    EmployeeDirectoryList.insert(END, line)
+file1.close()
 
 '''Buttons'''
-CreateOrder = Button(LowerLeftFrame, text="Create Order")
-GetReports = Button(LowerLeftFrame, text="Get Report")
-Misc = Button(LowerLeftFrame, text="Misc")
+CreateOrder = Button(LowerLeftFrame, text="Create Order", command=lambda: goToFile("SE Order.py"))
+GetReports = Button(LowerLeftFrame, text="Get Report", command=lambda: goToFile("posinventory.txt"))
 
 CreateOrder.pack(side=RIGHT, pady=5)
 GetReports.pack(side=RIGHT, padx=5, pady=5)
-Misc.pack(side=RIGHT, pady=5)
 
 Add = Button(LowerRightFrame, text="Add", command=lambda: addEmployee(EmployeeDirectoryList, eAdd))
 Remove = Button(LowerRightFrame, text="Remove", command=lambda: deleteEmployee(EmployeeDirectoryList))
 eAdd = Entry(LowerRightFrame)
+SaveEmployees = Button(LowerRightFrame, text="Save", command=lambda: saveEmployees())
 
 eAdd.pack(side=RIGHT, pady=5)
 Add.pack(side=RIGHT, padx=5, pady=5)
 Remove.pack(side=RIGHT, pady=5)
+SaveEmployees.pack(side=RIGHT, pady=5)
--- SE Order.py
+++ SE Order.py
@@ -1,10 +1,13 @@
 from tkinter import *
 
-def addToOrder(Lbox, entry1, entry2, entry3):
+def addToOrder(Lbox, entry, entry1, entry2, entry3):
+    e0 = entry.get()
     e1 = entry1.get()
     e2 = entry2.get()
     e3 = entry3.get()
     fullLine = ""
+    fullLine+=str(e0)
+    fullLine+="  "
     fullLine+=str(e1)
     fullLine+="  "
     fullLine+=str(e2)
@@ -15,6 +18,7 @@ def addToOrder(Lbox, entry1, entry2, entry3):
     orderfile = open("WeeklyOrder.txt", 'a')
     orderfile.write(fullLine)
     orderfile.close()
+    entry.delete(0,END)
     entry1.delete(0,END)
     entry2.delete(0,END)
     entry3.delete(0,END)
@@ -22,14 +26,12 @@ def addToOrder(Lbox, entry1, entry2, entry3):
 ##Broken at the moment
 
 def exportOrderToFile(Lbox):
-    '''
-    orderfile = open("WeeklyOrder.txt", 'w')
-    busString = ""
-    busString += Lbox.get(0,END)
-    orderfile.append(busString)
-    orderfile.close()
-    '''
-    Lbox.delete(0,END)
+    temp_list = list(CurrentOrder.get(0, END))
+    temp_list = [line + '\n' for line in temp_list]
+    fout = open("WeeklyOrder.txt", "w")
+    fout.writelines(temp_list)
+    fout.close()
+    CurrentOrder.delete(0, END)
 
 root = Tk()
 root.title("Order Form Submission")
@@ -51,18 +53,22 @@ Submit = Button(RightFrame, text="Submit Order", command=lambda: exportOrderToFi
 Submit.pack(side=BOTTOM,pady=10)
 
 '''RightFrame Junk'''
+lNum = Label(LeftFrame, text="ID")
+eNum = Entry(LeftFrame)
 lItem = Label(LeftFrame, text="Item")
 eItem = Entry(LeftFrame)
 lQuantity = Label(LeftFrame, text="Quantity")
 eQuantity = Entry(LeftFrame)
 lCost = Label(LeftFrame, text="Cost")
 eCost = Entry(LeftFrame)
-Add = Button(LeftFrame, text="Add", command=lambda: addToOrder(CurrentOrder, eItem, eQuantity, eCost))
+Add = Button(LeftFrame, text="Add", command=lambda: addToOrder(CurrentOrder, eNum, eItem, eQuantity, eCost))
 
-lItem.grid(row=0,column=0)
-eItem.grid(row=0,column=1)
-lQuantity.grid(row=1,column=0)
-eQuantity.grid(row=1,column=1)
-lCost.grid(row=2,column=0)
-eCost.grid(row=2,column=1)
-Add.grid(row=3,column=1,pady=10)
+lNum.grid(row=0,column=0)
+eNum.grid(row=0,column=1)
+lItem.grid(row=1,column=0)
+eItem.grid(row=1,column=1)
+lQuantity.grid(row=2,column=0)
+eQuantity.grid(row=2,column=1)
+lCost.grid(row=3,column=0)
+eCost.grid(row=3,column=1)
+Add.grid(row=4,column=1,pady=10)
